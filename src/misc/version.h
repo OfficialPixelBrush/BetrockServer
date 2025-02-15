b520a9a1d65a896403267a810762c2c2e5132f75
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-02-16 00:31:04 +0100

    fixed version number

--- src/misc/version.h
+++ src/misc/version.h
@@ -1,10 +1,10 @@
 // version.h.in
 #pragma once
 #define PROJECT_VERSION_MAJOR 0
-#define PROJECT_VERSION_MINOR 0
+#define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 13
-#define PROJECT_VERSION_STRING "0.0.13"
+#define PROJECT_VERSION_STRING "0.1.13"
 
 #define PROJECT_NAME "BetrockServer"
 
-#define PROJECT_NAME_VERSION "BetrockServer 0.0.13"
+#define PROJECT_NAME_VERSION "BetrockServer 0.1.13"
