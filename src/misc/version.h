8673b0e684c96c684b3426cfce030d8697964cee
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-02-24 14:19:48 +0100

    version number bump

--- src/misc/version.h
+++ src/misc/version.h
@@ -2,9 +2,9 @@
 #pragma once
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 1
-#define PROJECT_VERSION_PATCH 14
-#define PROJECT_VERSION_STRING "0.1.14"
+#define PROJECT_VERSION_PATCH 15
+#define PROJECT_VERSION_STRING "0.1.15"
 
 #define PROJECT_NAME "BetrockServer"
 
-#define PROJECT_NAME_VERSION "BetrockServer 0.1.14"
+#define PROJECT_NAME_VERSION "BetrockServer 0.1.15"
