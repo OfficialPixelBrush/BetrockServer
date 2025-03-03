996a36573cb672ddb6ea7f87f812938d4244d224
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-03-03 15:56:58 +0100

    Version bump

--- src/misc/version.h
+++ src/misc/version.h
@@ -2,11 +2,11 @@
 #pragma once
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 2
-#define PROJECT_VERSION_PATCH 5
-#define PROJECT_VERSION_STRING "0.2.5"
-#define PROJECT_VERSION_FULL_STRING "0.2.5 (latest/43434b1)"
+#define PROJECT_VERSION_PATCH 6
+#define PROJECT_VERSION_STRING "0.2.6"
+#define PROJECT_VERSION_FULL_STRING "0.2.6 (latest/db269a2)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "43434b1"
+#define PROJECT_GIT_COMMIT "db269a2"
 #define PROJECT_GIT_BRANCH "latest"
-#define PROJECT_NAME_VERSION "BetrockServer 0.2.5"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.5 (latest/43434b1)"
+#define PROJECT_NAME_VERSION "BetrockServer 0.2.6"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.6 (latest/db269a2)"
