eb6102361e408b0fd30ec81188cb861b839ed79d
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-03-05 22:27:26 +0100

    Bump to 0.2.8

--- src/misc/version.h
+++ src/misc/version.h
@@ -4,9 +4,9 @@
 #define PROJECT_VERSION_MINOR 2
 #define PROJECT_VERSION_PATCH 8
 #define PROJECT_VERSION_STRING "0.2.8"
-#define PROJECT_VERSION_FULL_STRING "0.2.8 (latest/3f0638f)"
+#define PROJECT_VERSION_FULL_STRING "0.2.8 (latest/a265cbc)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "3f0638f"
+#define PROJECT_GIT_COMMIT "a265cbc"
 #define PROJECT_GIT_BRANCH "latest"
 #define PROJECT_NAME_VERSION "BetrockServer 0.2.8"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.8 (latest/3f0638f)"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.8 (latest/a265cbc)"
