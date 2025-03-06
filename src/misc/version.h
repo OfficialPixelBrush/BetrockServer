1a78f9a9a64ff6333a5916c83d8d13abe1784bc0
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-03-06 13:48:12 +0100

    Never making a Windows port
    - I've decided to never make a Windows port. Cygwin doesn't work either. Doing it native is annoying and involves too much work.

--- src/misc/version.h
+++ src/misc/version.h
@@ -4,9 +4,9 @@
 #define PROJECT_VERSION_MINOR 2
 #define PROJECT_VERSION_PATCH 8
 #define PROJECT_VERSION_STRING "0.2.8"
-#define PROJECT_VERSION_FULL_STRING "0.2.8 (latest/ad42ba3)"
+#define PROJECT_VERSION_FULL_STRING "0.2.8 (latest/8490eb1)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "ad42ba3"
+#define PROJECT_GIT_COMMIT "8490eb1"
 #define PROJECT_GIT_BRANCH "latest"
 #define PROJECT_NAME_VERSION "BetrockServer 0.2.8"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.8 (latest/ad42ba3)"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.8 (latest/8490eb1)"
