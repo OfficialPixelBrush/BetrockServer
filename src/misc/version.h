52949f49da014af25d0d6aad5ae7312bcbb44879
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-05-19 10:56:47 +0200

    Removed unnecessary write to bug_report.md

--- src/misc/version.h
+++ src/misc/version.h
@@ -4,9 +4,9 @@
 #define PROJECT_VERSION_MINOR 2
 #define PROJECT_VERSION_PATCH 9
 #define PROJECT_VERSION_STRING "0.2.9"
-#define PROJECT_VERSION_FULL_STRING "0.2.9 (main/6870e4a)"
+#define PROJECT_VERSION_FULL_STRING "0.2.9 (latest/0453b76)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "6870e4a"
-#define PROJECT_GIT_BRANCH "main"
+#define PROJECT_GIT_COMMIT "0453b76"
+#define PROJECT_GIT_BRANCH "latest"
 #define PROJECT_NAME_VERSION "BetrockServer 0.2.9"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.9 (main/6870e4a)"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.9 (latest/0453b76)"
