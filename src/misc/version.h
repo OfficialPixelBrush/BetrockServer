aeb0e288fee05651453b102a35188ece316654b9
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-05-20 08:07:30 +0200

    Bump to 0.2.10

--- src/misc/version.h
+++ src/misc/version.h
@@ -2,11 +2,11 @@
 #pragma once
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 2
-#define PROJECT_VERSION_PATCH 9
-#define PROJECT_VERSION_STRING "0.2.9"
-#define PROJECT_VERSION_FULL_STRING "0.2.9 (latest/3c00a4a)"
+#define PROJECT_VERSION_PATCH 10
+#define PROJECT_VERSION_STRING "0.2.10"
+#define PROJECT_VERSION_FULL_STRING "0.2.10 (latest/062c185)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "3c00a4a"
+#define PROJECT_GIT_COMMIT "062c185"
 #define PROJECT_GIT_BRANCH "latest"
-#define PROJECT_NAME_VERSION "BetrockServer 0.2.9"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.9 (latest/3c00a4a)"
+#define PROJECT_NAME_VERSION "BetrockServer 0.2.10"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.10 (latest/062c185)"
