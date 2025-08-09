934afa22fa5ce7edd5c96a2e4b1c15116facced7
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-08-09 22:35:45 +0200

    wip

---
+++ src/plugins/terrain/historic/generatorBeta173.h
@@ -0,0 +1,21 @@
+#include "generator.h"
+
+// Implements Beta 1.7.3 Generation very closely
+
+class GeneratorBeta173 : public Generator {
+    private:
+        std::unique_ptr<JavaRandom> rand;
+        std::unique_ptr<InfdevOctaves> noiseGen1;
+        std::unique_ptr<InfdevOctaves> noiseGen2;
+        std::unique_ptr<InfdevOctaves> noiseGen3;
+        std::unique_ptr<InfdevOctaves> noiseGen4;
+        std::unique_ptr<InfdevOctaves> noiseGen5;
+        std::unique_ptr<InfdevOctaves> noiseGen6;
+        std::unique_ptr<InfdevOctaves> noiseGen7;
+        std::unique_ptr<InfdevOctaves> mobSpawnerNoise;
+    public:
+        GeneratorInfdev20100227(int64_t seed, World* world);
+        ~GeneratorInfdev20100227() = default;
+        std::unique_ptr<Chunk> GenerateChunk(int32_t cX, int32_t cZ) override;
+        bool PopulateChunk(int32_t cX, int32_t cZ) override;
+};
