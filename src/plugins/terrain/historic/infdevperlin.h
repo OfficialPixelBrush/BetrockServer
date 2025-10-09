e92a4684af445df4e490d2ac79bc752d914d237d
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-10-09 12:24:35 +0200

    Clean-up
    - Fixed typo in JavaRandom
    - Tweaked Perlin Noise to actually generate the Farlands ( :tada: :tada: :tada: )
    - Cleaned up Perlin Noise code to be much more readable

--- src/plugins/terrain/historic/infdevperlin.h
+++ src/plugins/terrain/historic/infdevperlin.h
@@ -2,7 +2,6 @@
 #pragma once
 
 #include "javaRandom.h"
-#include <cmath>
 
 class InfdevPerlin {
     private:
@@ -10,16 +9,17 @@ class InfdevPerlin {
         double xCoord;
         double yCoord;
         double zCoord;
-        double generateNoise(double xOffset, double yOffset, double zOffset);
+        double generateNoise(double x, double y, double z);
     public:
         InfdevPerlin();
         InfdevPerlin(JavaRandom* rand);
-        double generateNoise(double xOffset, double yOffset);
-        double generateNoiseD(double xOffset, double yOffset, double zOffset);
+        double generateNoise(double x, double y);
+        double generateNoiseD(double x, double y, double z);
 };
 
-static double lerp(double var0, double var2, double var4) {
-    return var2 + var0 * (var4 - var2);
+// Linear Interpolation
+static double lerp(double a, double b, double t) {
+    return b + a * (t - b);
 }
 
 static double grad(int var0, double var1, double var3, double var5) {
@@ -29,6 +29,7 @@ static double grad(int var0, double var1, double var3, double var5) {
     return ((var0 & 1) == 0 ? var8 : -var8) + ((var0 & 2) == 0 ? var10 : -var10);
 }
 
-static double staticGenerateNoise(double var0) {
-    return var0 * var0 * var0 * (var0 * (var0 * 6.0D - 15.0D) + 10.0D);
+// Easing Function
+static double fade(double value) {
+    return value * value * value * (value * (value * 6.0D - 15.0D) + 10.0D);
 }
