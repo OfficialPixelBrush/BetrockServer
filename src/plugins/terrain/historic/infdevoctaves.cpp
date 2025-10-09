3ded6c4637f359f587eaa880df9aed7f1c8e54f5
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-10-09 13:52:48 +0200

    Hacky but some progress

--- src/plugins/terrain/historic/infdevoctaves.cpp
+++ src/plugins/terrain/historic/infdevoctaves.cpp
@@ -37,3 +37,27 @@ double InfdevOctaves::generateNoiseOctaves(double xOffset, double yOffset, doubl
 
     return var7;
 }
+
+std::vector<double> InfdevOctaves::generateNoiseOctaves(std::vector<double> var1, double var2, double var4, double var6, int var8, int var9, int var10, double var11, double var13, double var15) {
+    if(var1.empty()) {
+        var1.reserve(var8 * var9 * var10);
+    } else {
+        for(int var17 = 0; var17 < var1.size(); ++var17) {
+            var1[var17] = 0.0D;
+        }
+    }
+
+    double var20 = 1.0D;
+
+    for(int var19 = 0; var19 < this->octaves; ++var19) {
+        // TODO: Add that
+        //this.generatorCollection[var19].func_646_a(var1, var2, var4, var6, var8, var9, var10, var11 * var20, var13 * var20, var15 * var20, var20);
+        var20 /= 2.0D;
+    }
+
+    return var1;
+}
+
+std::vector<double> InfdevOctaves::func_4103_a(std::vector<double> var1, int var2, int var3, int var4, int var5, double var6, double var8, double var10) {
+    return this->generateNoiseOctaves(var1, (double)var2, 10.0D, (double)var3, var4, 1, var5, var6, 1.0D, var8);
+}
--- src/plugins/terrain/historic/infdevperlin.h
+++ src/plugins/terrain/historic/infdevperlin.h
@@ -15,6 +15,7 @@ class InfdevPerlin {
         InfdevPerlin(JavaRandom* rand);
         double generateNoise(double x, double y);
         double generateNoiseD(double x, double y, double z);
+        void func_646_a(std::vector<double>& var1, double var2, double var4, double var6, int var8, int var9, int var10, double var11, double var13, double var15, double var17);
 };
 
 // Linear Interpolation
@@ -27,6 +28,13 @@ static double grad(int var0, double var1, double var3, double var5) {
     double var8 = var0 < 8 ? var1 : var3;
     double var10 = var0 < 4 ? var3 : (var0 != 12 && var0 != 14 ? var5 : var1);
     return ((var0 & 1) == 0 ? var8 : -var8) + ((var0 & 2) == 0 ? var10 : -var10);
+}
+
+static double altGrad(int var1, double var2, double var4) {
+    int var6 = var1 & 15;
+    double var7 = (double)(1 - ((var6 & 8) >> 3)) * var2;
+    double var9 = var6 < 4 ? 0.0D : (var6 != 12 && var6 != 14 ? var4 : var2);
+    return ((var6 & 1) == 0 ? var7 : -var7) + ((var6 & 2) == 0 ? var9 : -var9);
 }
 
 // Easing Function
