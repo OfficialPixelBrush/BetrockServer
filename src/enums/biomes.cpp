acb75673859ff8449a415dc9adced6f3f12a94ec
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-10-10 20:39:54 +0200

    Closer to real beta generation!
    - Improved function names
    - The Biome noises are now actually used
    - Biome Enum exists now
    - Terrain shapes are now generated
    - Replaced erroneous use of reserve with resize

---
+++ src/enums/biomes.cpp
@@ -0,0 +1,52 @@
+#include "biomes.h"
+
+Biome BiomeLUT[64*64];
+
+Biome GetBiome(float temperature, float humidity) {
+    humidity *= temperature;
+    if (temperature < 0.1) {
+        return BIOME_TUNDRA;
+    }
+    if (humidity < 0.2) {
+        if (temperature < 0.5) {
+            return BIOME_TUNDRA;
+        }
+        if (temperature < 0.95) {
+            return BIOME_SAVANNA;
+        }
+        return BIOME_DESERT;
+    }
+    if (humidity > 0.5 && temperature < 0.7) {
+        return BIOME_SWAMPLAND;
+    }
+    if (temperature < 0.5) {
+        return BIOME_TAIGA;
+    }
+    if (temperature < 0.97) {
+        if (humidity < 0.35) {
+            return BIOME_SHRUBLAND;
+        }
+        return BIOME_FOREST;
+    }
+    if (humidity < 0.45) {
+        return BIOME_PLAINS;
+    }
+    if (humidity < 0.9) {
+        return BIOME_SEASONALFOREST;
+    }
+    return BIOME_RAINFOREST;
+}
+
+void GenerateBiomeLookup() {
+    for(int temp = 0; temp < 64; ++temp) {
+        for(int humi = 0; humi < 64; ++humi) {
+            BiomeLUT[temp + humi * 64] = GetBiome((float)temp / 63.0F, (float)humi / 63.0F);
+        }
+    }
+}
+
+Biome GetBiomeFromLookup(float temperature, float humidity) {
+    int temp = (int)(temperature * 63.0D);
+    int humi = (int)(humidity * 63.0D);
+    return BiomeLUT[temp + humi * 64];
+}
--- src/plugins/terrain/historic/generatorBeta173.h
+++ src/plugins/terrain/historic/generatorBeta173.h
@@ -1,8 +1,10 @@
 #include "generator.h"
+#include "biomes.h"
 
 class GeneratorBeta173 : public Generator {
     private:
         std::unique_ptr<JavaRandom> rand;
+        // Perlin Noise Generators
         std::unique_ptr<NoiseOctaves<NoisePerlin>> noiseGen1;
         std::unique_ptr<NoiseOctaves<NoisePerlin>> noiseGen2;
         std::unique_ptr<NoiseOctaves<NoisePerlin>> noiseGen3;
@@ -11,14 +13,29 @@ class GeneratorBeta173 : public Generator {
         std::unique_ptr<NoiseOctaves<NoisePerlin>> noiseGen6;
         std::unique_ptr<NoiseOctaves<NoisePerlin>> noiseGen7;
         std::unique_ptr<NoiseOctaves<NoisePerlin>> mobSpawnerNoise;
-        void GenerateTerrain(int cX, int cY, std::unique_ptr<Chunk>& c, std::vector<double>& temperature);
-        std::vector<double> GenerateTerrainNoise(std::vector<double> var1, int var2, int var3, int var4, int var5, int var6, int var7);
-        std::vector<double> field_4224_q;
-        std::vector<double> field_4229_d;
-        std::vector<double> field_4228_e;
-        std::vector<double> field_4227_f;
-        std::vector<double> field_4226_g;
-        std::vector<double> field_4225_h;
+
+        // Simplex Noise Generators
+        std::unique_ptr<NoiseOctaves<NoiseSimplex>> noiseSimplex1;
+        std::unique_ptr<NoiseOctaves<NoiseSimplex>> noiseSimplex2;
+        std::unique_ptr<NoiseOctaves<NoiseSimplex>> noiseSimplex3;
+
+        // Stored noise
+        std::vector<double> terrainNoise;
+        std::vector<double> noiseField1;
+        std::vector<double> noiseField2;
+        std::vector<double> noiseField3;
+        std::vector<double> noiseField4;
+        std::vector<double> noiseField5;
+
+        // Biome Vectors
+        std::vector<Biome> biomeMap;
+        std::vector<double> temperature;
+        std::vector<double> humidity;
+        std::vector<double> otherBiomeThing;
+
+        void GenerateTerrain(int cX, int cY, std::unique_ptr<Chunk>& c, std::vector<Biome> biomeMap, std::vector<double>& temperature);
+        std::vector<double> GenerateTerrainNoise(std::vector<double> terrainMap, int x, int y, int z, int xMax, int yMax, int zMax);
+        std::vector<Biome> GenerateBiomeMap(std::vector<Biome> biomeMap, int x, int z, int xMax, int zMax);
     public:
         GeneratorBeta173(int64_t seed, World* world);
         ~GeneratorBeta173() = default;
