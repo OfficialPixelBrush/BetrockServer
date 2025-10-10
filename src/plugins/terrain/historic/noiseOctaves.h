048a29c4838d8becd9ffcd4a06b3a4cde46500ad
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-10-10 09:51:40 +0200

    Adding a few missing functions

--- src/plugins/terrain/historic/noiseOctaves.h
+++ src/plugins/terrain/historic/noiseOctaves.h
@@ -15,6 +15,8 @@ class NoiseOctaves {
         double GenerateOctaves(double xOffset, double yOffset, double zOffset);
         std::vector<double> GenerateOctaves(std::vector<double> var1, double var2, double var4, double var6, int var8, int var9, int var10, double var11, double var13, double var15);
         std::vector<double> GenerateOctaves(std::vector<double> var1, int var2, int var3, int var4, int var5, double var6, double var8, double var10);
+        std::vector<double> GenerateOctaves(std::vector<double> var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12);
+        std::vector<double> GenerateOctaves(std::vector<double> var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12, double var14);
     private:
         int octaves;
         std::vector<std::unique_ptr<T>> generatorCollection;
@@ -76,8 +78,7 @@ std::vector<double> NoiseOctaves<T>::GenerateOctaves(std::vector<double> var1, d
     double var20 = 1.0D;
 
     for(int var19 = 0; var19 < this->octaves; ++var19) {
-        // TODO: Add that
-        //this.generatorCollection[var19].func_646_a(var1, var2, var4, var6, var8, var9, var10, var11 * var20, var13 * var20, var15 * var20, var20);
+        this->generatorCollection[var19]->GenerateNoise(var1, var2, var4, var6, var8, var9, var10, var11 * var20, var13 * var20, var15 * var20, var20);
         var20 /= 2.0D;
     }
 
@@ -87,4 +88,33 @@ std::vector<double> NoiseOctaves<T>::GenerateOctaves(std::vector<double> var1, d
 template <typename T>
 std::vector<double> NoiseOctaves<T>::GenerateOctaves(std::vector<double> var1, int var2, int var3, int var4, int var5, double var6, double var8, double var10) {
     return this->GenerateOctaves(var1, (double)var2, 10.0D, (double)var3, var4, 1, var5, var6, 1.0D, var8);
+}
+
+template <typename T>
+std::vector<double> NoiseOctaves<T>::GenerateOctaves(std::vector<double> var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12) {
+    return this->GenerateOctaves(var1, var2, var4, var6, var7, var8, var10, var12, 0.5D);
+}
+
+template <typename T>
+std::vector<double> NoiseOctaves<T>::GenerateOctaves(std::vector<double> var1, double var2, double var4, int var6, int var7, double var8, double var10, double var12, double var14) {
+    var8 /= 1.5D;
+    var10 /= 1.5D;
+    if(!var1.empty() && var1.size() >= var6 * var7) {
+        for(int var16 = 0; var16 < var1.size(); ++var16) {
+            var1[var16] = 0.0D;
+        }
+    } else {
+        var1.reserve(var6 * var7);
+    }
+
+    double var21 = 1.0D;
+    double var18 = 1.0D;
+
+    for(int var20 = 0; var20 < this->octaves; ++var20) {
+        this->generatorCollection[var20]->GenerateNoise(var1, var2, var4, var6, var7, var8 * var18, var10 * var18, 0.55D / var21);
+        var18 *= var12;
+        var21 *= var14;
+    }
+
+    return var1;
 }
