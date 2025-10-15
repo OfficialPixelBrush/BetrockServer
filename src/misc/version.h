bf1226081d0c6184408e132b3d4c292a9eabc357
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-10-15 14:20:42 +0200

    Some tweaks
    - Debump from 0.3.0 to 0.2.12
    - Fixed major issue where folders would refuse to be created
    - World generator now defaults to infdev20100327 generator
    - Default view distance is now 10
    -

--- src/misc/version.h
+++ src/misc/version.h
@@ -1,12 +1,12 @@
 // version.h.in
 #pragma once
 #define PROJECT_VERSION_MAJOR 0
-#define PROJECT_VERSION_MINOR 3
-#define PROJECT_VERSION_PATCH 0
-#define PROJECT_VERSION_STRING "0.3.0"
-#define PROJECT_VERSION_FULL_STRING "0.3.0 (latest/b0b1b8d)"
+#define PROJECT_VERSION_MINOR 2
+#define PROJECT_VERSION_PATCH 12
+#define PROJECT_VERSION_STRING "0.2.12"
+#define PROJECT_VERSION_FULL_STRING "0.2.12 (latest/ca984a0)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "b0b1b8d"
+#define PROJECT_GIT_COMMIT "ca984a0"
 #define PROJECT_GIT_BRANCH "latest"
-#define PROJECT_NAME_VERSION "BetrockServer 0.3.0"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.3.0 (latest/b0b1b8d)"
+#define PROJECT_NAME_VERSION "BetrockServer 0.2.12"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.2.12 (latest/ca984a0)"
