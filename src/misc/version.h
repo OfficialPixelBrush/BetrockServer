25ff4cdcbb03ed0b2c25a9b65be0ce0cd2835695
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2025-11-27 11:51:11 +0100

    PixNBT update to fix shadowing

--- src/misc/version.h
+++ src/misc/version.h
@@ -4,9 +4,9 @@
 #define PROJECT_VERSION_MINOR 3
 #define PROJECT_VERSION_PATCH 0
 #define PROJECT_VERSION_STRING "0.3.0"
-#define PROJECT_VERSION_FULL_STRING "0.3.0 (latest/c6de941)"
+#define PROJECT_VERSION_FULL_STRING "0.3.0 (latest/7f6c06c)"
 #define PROJECT_NAME "BetrockServer"
-#define PROJECT_GIT_COMMIT "c6de941"
+#define PROJECT_GIT_COMMIT "7f6c06c"
 #define PROJECT_GIT_BRANCH "latest"
 #define PROJECT_NAME_VERSION "BetrockServer 0.3.0"
-#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.3.0 (latest/c6de941)"
+#define PROJECT_NAME_VERSION_FULL "BetrockServer 0.3.0 (latest/7f6c06c)"
